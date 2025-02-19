5324701a6c0c8df88a79b95ae43cd701dd842e72
KhadijaMez <khadija.mezghanni@esprit.tn>
2025-02-19 15:02:45 +0100

    commit

--- src/main/java/tn/coconsultbackend/QualityManagementProcess/Repositories/QualityManagementProcessRepository.java
+++ src/main/java/tn/coconsultbackend/QualityManagementProcess/Repositories/QualityManagementProcessRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tn.coconsultbackend.Model.Process;
 
 public interface QualityManagementProcessRepository extends JpaRepository<Process,Long> {
+    //vhhvgjhlfkflflcfjfl
 }
