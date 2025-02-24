b9d44d15c5f2ebed696f11de0249813650134eb1
Iyed519 <omri.iyed@esprit.tn>
2025-02-24 13:11:36 +0100

    running spring security

--- src/main/java/tn/coconsultbackend/CoconsultBackendApplication.java
+++ src/main/java/tn/coconsultbackend/CoconsultBackendApplication.java
@@ -1,9 +1,13 @@
 package tn.coconsultbackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+import tn.coconsultbackend.role.Role;
+import tn.coconsultbackend.role.RoleRepository;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -14,4 +18,14 @@ public class CoconsultBackendApplication {
         SpringApplication.run(CoconsultBackendApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner runner(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.findByName("USER").isEmpty()){
+                roleRepository.save(
+                        Role.builder().name("USER").build()
+                );
+            }
+        };
+    }
 }
