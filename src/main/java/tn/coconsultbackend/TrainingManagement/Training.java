a165416f9554d31e9011162e92535145bb2e92c8
Iyed519 <omri.iyed@esprit.tn>
2025-02-27 20:15:20 +0100

    debut

--- src/main/java/tn/coconsultbackend/TrainingManagement/Training.java
+++ src/main/java/tn/coconsultbackend/TrainingManagement/Training.java
@@ -1,4 +1,25 @@
 package tn.coconsultbackend.TrainingManagement;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Training {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotN
+    private String title;
+
 }
